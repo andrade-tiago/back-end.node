542b598be1e8c2b204c8c3ccdc384aa9d2871a24
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-05-30 19:28:45 -0300

    :wrench: chore: add some error message constants

---
+++ src/constants/error-messages.ts
@@ -0,0 +1,11 @@
+export enum ErrorMessages {
+  EmailAlreadyUsed = 'E-mail already used',
+
+  InvalidEmail = 'Invalid e-mail',
+  InvalidCPF = 'Invalid CPF',
+
+  LoginError = 'Invalid e-mail or password',
+
+  UserNotFound = 'User not found',
+  UserAlreadyDeleted = 'User already deleted',
+}
