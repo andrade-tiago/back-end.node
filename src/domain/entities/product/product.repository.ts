724189f9eb42d8525fb58a66f66d56e768c33ec7
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-06-20 18:57:18 -0300

    :wrench: chore(product repository contract): add get many method

--- src/domain/entities/product/product.repository.ts
+++ src/domain/entities/product/product.repository.ts
@@ -4,6 +4,7 @@ export interface IProductRepository {
   add(product: Product): Promise<void>;
   getById(id: Product['id']): Promise<Product | undefined>;
   getPaged(options: ProductGetPagedOptions): Promise<Product[]>;
+  getMany(ids: Array<Product['id']>): Promise<Product[]>;
 }
 
 export type ProductGetPagedOptions = {
