16465cfe3454d10173bd42d8e391f61343617638
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-06-24 18:53:05 -0300

    :sparkles: feat: add product mapper implementation

---
+++ src/application/mappers/implementations/product-mapper.ts
@@ -0,0 +1,15 @@
+import { ProductOutput } from "@/application/use-cases/product.output";
+import { Product } from "@/domain/entities/product";
+import { IProductMapper } from "../product.mapper";
+
+export class ProductMapper implements IProductMapper {
+  public toOutput(product: Product): ProductOutput {
+    return {
+      id: product.id.value,
+      title: product.title.value,
+      price: product.price.value,
+      inStock: product.inStock.value,
+      createdAt: new Date(product.createdAt.value).toISOString(),
+    };
+  }
+}
