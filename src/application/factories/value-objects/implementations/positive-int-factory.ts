1aad222c3cb185ecfd5457feee75056fbdd0f305
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-06-24 18:45:38 -0300

    :sparkles: feat: add positive int factory implementation

---
+++ src/application/factories/value-objects/implementations/positive-int-factory.ts
@@ -0,0 +1,8 @@
+import { PositiveInt } from "@/domain/shared/value-objects/positive-int.vo";
+import { IPositiveIntFactory } from "./positive-int.factory";
+
+export class PositiveIntFactory implements IPositiveIntFactory {
+  public create(value: number): PositiveInt {
+    return new PositiveInt(value);
+  }
+}
