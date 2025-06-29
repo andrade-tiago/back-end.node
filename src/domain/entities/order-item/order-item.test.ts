1d78d8126fee855f23e0bdd162159c331bb3b4eb
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-06-29 16:47:12 -0300

    :pencil: docs(OrderItem test): replace VO with Entity where it refers to OrderItem

--- src/domain/entities/order-item/order-item.test.ts
+++ src/domain/entities/order-item/order-item.test.ts
@@ -4,7 +4,7 @@ import { Uuid } from "@/domain/shared/value-objects/uuid.vo";
 import { PositiveInt } from "@/domain/shared/value-objects/positive-int.vo";
 import { Monetary } from "@/domain/shared/value-objects/monetary.vo";
 
-describe('OrderItem VO', () => {
+describe('OrderItem Entity', () => {
   it('should be created with accessible and correct values', () => {
     const uuid = new Uuid('95d58700-fe12-4fa6-9c8e-a2b9b353cda5');
     const price = new Monetary(12.5);
